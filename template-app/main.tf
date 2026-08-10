b261c2e93b8402de130dc7422f1f9d84986c1a08
Robert Puffe <robertpuffe@gmail.com>
2026-08-09 21:07:21 -0500

    Prepare Flightdeck v0.7.2

--- template-app/main.tf
+++ template-app/main.tf
@@ -67,7 +67,7 @@ locals {
 }
 
 module "app" {
-  source = "git::https://github.com/rpuffe/flightdeck.git//modules/fargate-service?ref=v0.7.1"
+  source = "git::https://github.com/rpuffe/flightdeck.git//modules/fargate-service?ref=v0.7.2"
 
   name             = local.manifest.name
   port             = local.manifest.port
